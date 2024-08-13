2f298b2fbc4c83f132a73788acbd2582e90e47b2
ademide245 <abubakaradedayo088@gmail.com>
2024-08-13 05:51:32 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+let menuIcon = document.querySelector("#menu-icon")
+let navbar=document.querySelector(".navbar")
+menuIcon.onclick=()=>{menuIcon.classList.toggle("bx-x")
+navbar.classList.toggle("active")}
